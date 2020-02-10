3cf0ef111377a00c5c9b5a01e939844200acddf6
solari-kn <let4it1be5@gmail.com>
2020-02-10 21:55:27 +0900

    スライドショー

--- script.js
+++ script.js
@@ -13,22 +13,42 @@ $(function() {
   });
 });
 
+$(function () {
+  $("#slider").slick({
+    customPaging: function(slider, i) {
+      return $("<div>");
+    },
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4500,
+    speed: 800,
+    pauseOnHover: false,
+    slidesToShow: 1,
+    fade: true,
+    slide: ".header_bg_slide",
+    dotsClass: 'slide-dots',
+    zIndex: 1
+  });
+})
+
 // ヘッダーインナーの高さ
 var pcHeight = 180;
 var spHeight = 180;
 
 $(window).on("resize", function() {
   winW = $(window).outerWidth();
-  winH = $(window).outerHeight();
+  winH = $(window).height();
+  outH = $(window).outerHeight();
   scrH = screen.height;
   spH = scrH - 50;
   if (winW > 767) {
-    $(".header_bg").css("height", winH - pcHeight + "px");
+    $(".header_bg_slide").css("height", winH - pcHeight + "px");
     $(".header_nav").css("height", winH - pcHeight + "px");
     $(".header_inner").css("height", pcHeight + "px");
   } else {
-    $(".header_bg").css("height", winH - spHeight + "px");
-    $(".header_nav").css("height", winH + "px");
+    $(".header_bg_slide").css("height", winH - spHeight + "px");
+    $(".header_nav").css("height", outH + "px");
     $(".header_inner").css("height", spHeight + "px");
   }
 });
@@ -37,41 +57,27 @@ $(function() {
   winW = $(window).outerWidth();
   winH = $(window).outerHeight();
   if (winW > 767) {
-    $(".header_bg").css("height", winH - pcHeight + "px");
+    $(".header_bg_slide").css("height", winH - pcHeight + "px");
     $(".header_nav").css("height", winH - pcHeight + "px");
     $(".header_inner").css("height", pcHeight + "px");
   } else {
-    $(".header_bg").css("height", winH - spHeight + "px");
+    $(".header_bg_slide").css("height", winH - spHeight + "px");
     $(".header_nav").css("height", winH + "px");
     $(".header_inner").css("height", spHeight + "px");
   }
 });
 
-// $(window).on("scroll", function() {
-//   scT = $(window).scrollTop();
-//   winH = $(window).height();
-//   if (scT > 0) {
-//     $(".header_nav").addClass("scrl");
-//     $(".scrl").css("height", winH + "px");
-//   } else {
-//     $(".header_nav").removeClass("scrl");
-//   }
-// });
-// $(window).on("resize", function() {
-//   winW = $(window).outerWidth();
-//   winH = $(window).height();
-//   if (winW > 767) {
-
-//   } else {
-//   }
-// });
-
-// $(function() {
-//   winW = $(window).outerWidth();
-//   winH = $(window).height();
-//   if (winW > 767) {
-//   } else {
-//   }
-// });
+$(window).on("scroll", function() {
+  winW = $(window).outerWidth();
+  scT = $(window).scrollTop();
+  winH = $(window).height();
+  if (scT > 0 && winW > 767) {
+    $(".header_nav").addClass("scrl");
+    $(".scrl").css("height", winH + "px");
+  } else if (winW > 767) {
+    $(".header_nav").removeClass("scrl");
+    $(".header_nav").css("height", winH - spHeight + "px");
+  }
+});
 
 // もし少しでもスクロールしたらポジションをfixed 高さをスクリーンいっぱいにする
