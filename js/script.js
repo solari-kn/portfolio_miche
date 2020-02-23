ff64f9ac7ad297d88011e6ab366a567309f96910
solari-kn <let4it1be5@gmail.com>
2020-02-23 17:09:47 +0900

    ローディング完成

--- js/script.js
+++ js/script.js
@@ -25,19 +25,20 @@ $(function() {
 let pcHeight = 140,
   spHeight = 180,
   pcglHeight = 80,
-  spglHeight = 60,
-  winW = $(window).outerWidth(),
-  winH = $(window).height(),
-  outH = $(window).outerHeight(),
-  scrH = screen.height;
+  spglHeight = 60;
 
 $(window).on("resize", function() {
+  let winW = $(window).outerWidth(),
+    winH = $(window).height(),
+    outH = $(window).outerHeight();
+  // scrH = screen.height;
+  // spH = scrH - 50;
   if (winW > 959) {
     $(".header_bg_slide").css("height", winH - pcHeight + "px");
     $(".header_nav").css("height", winH - pcHeight + "px");
     $(".glbg").css("height", outH - pcglHeight + "px");
     $(".glHeader_nav").css("height", winH - 80 + "px");
-    $(".glHeader_cover, .loading").css("height", winH + "px");
+    $(".glHeader_cover").css("height", winH + "px");
     $(".header_inner").css("height", pcHeight + "px");
   } else {
     $(".header_bg_slide").css("height", winH - spHeight + "px");
@@ -49,24 +50,28 @@ $(window).on("resize", function() {
 });
 
 $(function() {
+  winW = $(window).outerWidth();
+  winH = $(window).outerHeight();
   if (winW > 959) {
-    $(".header_bg_slide").css("height", outH - pcHeight + "px");
-    $(".header_nav").css("height", outH - pcHeight + "px");
-    $(".glbg").css("height", outH - pcglHeight + "px");
-    $(".glHeader_nav").css("height", outH - 80 + "px");
-    $(".glHeader_cover, .loading").css("height", outH + "px");
+    $(".header_bg_slide").css("height", winH - pcHeight + "px");
+    $(".header_nav").css("height", winH - pcHeight + "px");
+    $(".glbg").css("height", winH - pcglHeight + "px");
+    $(".glHeader_nav").css("height", winH - 80 + "px");
+    $(".glHeader_cover").css("height", winH + "px");
     $(".header_inner").css("height", pcHeight + "px");
   } else {
-    $(".header_bg_slide").css("height", outH - spHeight + "px");
-    $(".header_nav").css("height", outH + "px");
-    $(".glbg").css("height", outH - 300 + "px");
-    $(".glHeader_nav").css("height", outH + "px");
+    $(".header_bg_slide").css("height", winH - spHeight + "px");
+    $(".header_nav").css("height", winH + "px");
+    $(".glbg").css("height", winH - 300 + "px");
+    $(".glHeader_nav").css("height", winH + "px");
     $(".header_inner").css("height", spHeight + "px");
   }
 });
 
 $(window).on("scroll", function() {
-  let scT = $(window).scrollTop();
+  winW = $(window).outerWidth();
+  scT = $(window).scrollTop();
+  winH = $(window).height();
   if (scT > 0 && winW > 959) {
     $(".header_nav, .header_cover").addClass("scrl");
     $(".scrl").css("height", winH + "px");
@@ -77,14 +82,15 @@ $(window).on("scroll", function() {
   }
 });
 
+
+
 $(function() {
   let $content = $(".container"),
     $loadBg = $(".loading"),
-    $loadWrap = $(".loading_wrap"),
     $slider = $("#slider"),
     $bar = $(".loading_wrap_line"),
     // imagesLoadedオブジェクトを作成して格納
-    imgLoad = imagesLoaded("body"),
+    imgLoad = imagesLoaded(".load", { background: true }),
     // body全体の画像の枚数を格納
     imgTotal = imgLoad.images.length,
     //読み込んだ画像の枚数を格納
@@ -92,9 +98,10 @@ $(function() {
     //プログレスバー現在地に相当する値を格納( =画像読込進捗度)
     current = 0,
     // 1秒当たり20回の頻度で読込状況をチェック
-    progressTimer = setInterval(updateProgress, 1000 / 20);
+    progressTimer = setInterval(updateProgress, 1000 / 30);
 
   $content.hide();
+
   imgLoad.on("progress", function() {
     imgLoaded++;
   });
@@ -102,82 +109,45 @@ $(function() {
   function updateProgress() {
     let target = (imgLoaded / imgTotal) * 100;
 
-    current += (target - current) * 0.1;
+    current += (target - current) * 0.2;
 
     $bar.css({ width: current + "%" });
 
     if (current >= 100) {
       clearInterval(progressTimer);
-      $content.show("0", function() {
+      $content.show("0", function () {
         if ($slider.length) {
-          $slider.slick("setPosition");
+          $slider.slick({
+            customPaging: function(slider, i) {
+              return $("<div>");
+            },
+            arrows: false,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            speed: 1800,
+            pauseOnHover: false,
+            slidesToShow: 1,
+            fade: true,
+            slide: ".header_bg_slide",
+            dotsClass: "slide-dots",
+            zIndex: 1,
+            waitForAnimate: false
+          });
         }
-        $loadWrap.fadeOut("500");
-        $loadBg.animate({ right: "-100%" }, 1000, "easeInOutQuint");
+        $loadBg.addClass("hide")
+        // $loadWrap.fadeOut("slow");
+        // $loadBg.animate({ right: "-100%" }, 800, "easeInOutQuint");
       });
     }
     if (current > 99.9) current = 100;
   }
 });
 
-/*
-$(function() {
-  let $content = $(".container"),
-    $loadBg = $(".loading"),
-    $loadWrap = $(".loading_wrap"),
-    $slider = $("#slider"),
-    $bar = $(".loading_wrap_line"),
-    // imagesLoadedオブジェクトを作成して格納
-    imgLoad = $content.imagesLoaded,
-    // body全体の画像の枚数を格納
-    imgTotal = imgLoad().images.length,
-    //読み込んだ画像の枚数を格納
-    imgLoaded = 0,
-    //プログレスバー現在地に相当する値を格納( =画像読込進捗度)
-    current = 0,
-    // 1秒当たり20回の頻度で読込状況をチェック
-    progressTimer = setInterval(updateProgress, 1000 / 20);
-
-  $content.hide();
-  imgLoad().progress( function() {
-    imgLoaded++;
-  });
-
-  function updateProgress() {
-    let target = (imgLoaded / imgTotal) * 100;
-
-    current += (target - current) * 0.1;
-
-    $bar.css({ width: current + "%" });
-
-    if (current >= 100) {
-      clearInterval(progressTimer);
-      $content.show("0", function() {
-        if ($slider.length) {
-          $slider.slick("setPosition");
-        }
-        $loadWrap.fadeOut("300");
-        $loadBg.animate({ right: "-100%" }, 1000, "easeInOutQuint");
-      });
-    }
-    if (current > 99.9) current = 100;
-  }
-}); */
-/* $(function() {
-  var content = $(".glContainer");
-  content.hide();
-  content.imagesLoaded({ background: true }, function() {
-    content.delay("1000").show("0", function() {
-      // slider.slick("setPosition");
-      $(".loading_wrap").fadeOut("300", function() {
-        $(".loading").animate({right: "-100%"},
-          500,
-        );
-      });
-    });
-  });
-}); */
-/* function() {
-        $(".wrap").fadeIn("20")
-        $("#slider").slick("setPosition");
-      }) */
+imagesLoaded(".load", { background: true }).on("progress", function(
+  instance,
+  image
+) {
+  const result = image.isLoaded ? "読み込まれました" : "壊れています";
+  console.log('${image.img.src}は${result}');
+});
